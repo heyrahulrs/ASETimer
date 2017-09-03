5f211b13e0415644ea95e931baf95d5913cebf1a
rahulrs0029 <rahulrs0029@gmail.com>
2017-09-03 11:07:07 +0530

    Added countdown timer for Apple Special Event 2017 & Added Widget

--- ASE Timer Widget/TodayViewController.swift
+++ ASE Timer Widget/TodayViewController.swift
@@ -1,17 +1,18 @@
 //
-//  MainViewController.swift
-//  ASE Timer
+//  TodayViewController.swift
+//  ASE Timer Widget
 //
 //  Created by Rahul Sharma on 03/09/17.
 //  Copyright © 2017 Rahul Sharma. All rights reserved.
 //
 
 import UIKit
+import NotificationCenter
 
-class MainViewController: UIViewController {
+class TodayViewController: UIViewController, NCWidgetProviding {
     
     @IBOutlet weak var countdownTimerLabel: UILabel!
-
+        
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -37,12 +38,20 @@ class MainViewController: UIViewController {
         }
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
 }
-
