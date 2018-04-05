9c54b9e1290fe7a5f9ec565d1aa6f0f61219bb3b
Rahul <rahulrs0029@gmail.com>
2018-04-05 16:27:17 +0530

    Minor Improvements

--- ASE Timer/MainViewController.swift
+++ ASE Timer/MainViewController.swift
@@ -17,21 +17,21 @@ class MainViewController: UIViewController {
 
         let eventUnixTime: TimeInterval = 1528131600
         
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { Void in
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
             
             let currentUnixTime = Date().timeIntervalSince1970
             
-            let timer: Double = eventUnixTime - currentUnixTime
+            let secondsUntilEvent: Double = eventUnixTime - currentUnixTime
             
-            if timer <= 0 {
-                self.countdownTimerLabel.text = "Watch the event live at apple.co/live"
+            if secondsUntilEvent <= 0 {
+                self.countdownTimerLabel.text = "Keynote is now streaming live."
                 return
             }
             
-            let days = Int(timer / 86400)
-            let hours = Int(timer.truncatingRemainder(dividingBy: 86400) / 3600)
-            let minutes = Int(timer.truncatingRemainder(dividingBy: 3600) / 60)
-            let seconds = Int(timer.truncatingRemainder(dividingBy: 60))
+            let days = Int(secondsUntilEvent / 86400)
+            let hours = Int(secondsUntilEvent.truncatingRemainder(dividingBy: 86400) / 3600)
+            let minutes = Int(secondsUntilEvent.truncatingRemainder(dividingBy: 3600) / 60)
+            let seconds = Int(secondsUntilEvent.truncatingRemainder(dividingBy: 60))
             
             self.countdownTimerLabel.text = "\(days)d \(hours)h \(minutes)m \(seconds)s"
         }
