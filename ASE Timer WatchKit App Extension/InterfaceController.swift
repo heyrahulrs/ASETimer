7eec328e00e86d40f491c73b2cd5a15775f6af5f
Rahul <rahulrs0029@gmail.com>
2019-05-17 15:24:17 +0530

    Fixed a bug in WatchKit App Extension.

--- ASE Timer WatchKit App Extension/InterfaceController.swift
+++ ASE Timer WatchKit App Extension/InterfaceController.swift
@@ -7,7 +7,6 @@
 //
 
 import WatchKit
-import Foundation
 
 class InterfaceController: WKInterfaceController {
     
@@ -16,9 +15,11 @@ class InterfaceController: WKInterfaceController {
     @IBOutlet weak var eventTitleLabel: WKInterfaceLabel!
 
     @IBOutlet weak var timeLeftTimer: WKInterfaceTimer!
+    @IBOutlet weak var infoLabel: WKInterfaceLabel!
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
+        infoLabel.setText("")
     }
     
     override func willActivate() {
@@ -41,8 +42,21 @@ class InterfaceController: WKInterfaceController {
         
         guard let eventUnixTime = event.unixTime else { return }
         
+        if eventUnixTime + 7200.0 < Date().timeIntervalSince1970  {
+            infoLabel.setText("Event has been councluded.")
+            timeLeftTimer.setHidden(true)
+            return
+        } else if eventUnixTime < Date().timeIntervalSince1970  {
+            infoLabel.setText("Event is now streaming.")
+            timeLeftTimer.setHidden(true)
+            return
+        }
+        
         let eventDate = Date(timeIntervalSince1970: eventUnixTime)
         timeLeftTimer.setDate(eventDate)
         
+        infoLabel.setHidden(true)
+
     }
+    
 }
